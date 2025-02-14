226cd3ff43f148d0ac6d4d65a99546bc2118b18f
Danilo <alaskawebsites94@gmail.com>
2025-02-14 19:30:01 -0300

    Subindo att

--- assets/js/index.js
+++ assets/js/index.js
@@ -119,10 +119,14 @@ galleryImages.forEach((img, index) => {
 
 // Atualiza a imagem no modal
 function updateModalImage() {
-  modalImage.src = galleryImages[currentImageIndex].src;
-  modalProgress.textContent = `Imagem ${currentImageIndex + 1} de ${
-    galleryImages.length
-  }`;
+  modalImage.style.opacity = 0;
+  setTimeout(() => {
+    modalImage.src = galleryImages[currentImageIndex].src;
+    modalImage.style.opacity = 1;
+    modalProgress.textContent = `Imagem ${currentImageIndex + 1} de ${
+      galleryImages.length
+    }`;
+  }, 300);
 }
 
 // Botão "Anterior"
@@ -140,8 +144,7 @@ document.querySelector("#nextImage").addEventListener("click", () => {
 
 // Navegação por teclado no modal
 document.addEventListener("keydown", (event) => {
-  if (galleryModal._element.classList.contains("show")) {
-    // Verifica se o modal está aberto
+  if (galleryModal._isShown) {
     if (event.key === "ArrowLeft") {
       currentImageIndex =
         (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
